604140ea1b5e7878b2dbed73049f1d0828d56387
Amelia <ameliagarvin1@gmail.com>
2017-10-23 13:41:04 -0700

    Updated styles, included media queries, Javascript refactoring with assistance

--- js/main.js
+++ js/main.js
@@ -1,34 +1,54 @@
 $(function() { 
 
- $('#sections').on('change', function() {
+  //when user clicks on the sections drop down menu it searches the NYT API// 
+
+  $('#sections').on('change', function() {
+
+    $('#articles').empty();
+    
+   $('.loading').show(); // If user clicks on sections drop menu the loader will appear //
+
    var clickon = $('#sections').val(); 
-   var url = "https://api.nytimes.com/svc/topstories/v2/" + clickon + ".json";
-   url += '?' + $.param({
+   var apiUrl = "https://api.nytimes.com/svc/topstories/v2/" + clickon + ".json";
+   apiUrl += '?' + $.param({
     'api-key': "477581d880d94e029c1543365b5b4c84"
 });
 
-
-
+// This function is going through the NYT API traversing the DOM and grabbin the 4th value in the multimedia index//
 $.ajax({
-    url: url,
+    url: apiUrl,
     method: 'GET',
 }).done(function(data) {
-    // console.log(data);
-    var resultsFromObject = data.results;
-  $.each(resultsFromObject, function(index, value) {
-  //  console.log(value.multimedia)
-   console.log(value);
-   $('.news-items').append('<li><img src="' + value.multimedia[4].url + '"></li>');
-  });  
 
+var resultsFromObject = data.results.slice(0,12);
+ 
+$.each(resultsFromObject, function(index, value) {
 
+    // console.log(value.multimedia);
+    
+    if( value.multimedia.length){
+        var output = '<div class="article-item" style="background-image:url(';
+        output += value.multimedia[3].url;
+        output += ')">';
+        output += value.abstract;       
+        output += '</div>';
+    
+        // "<li>" + value.abstract + '<div class=".news-items" style"background-image:url(' + value.multimedia[4].url + ")'> "</li>"'</div>’);
+    
+    $('#articles').append(output);
+    }
+    
 
-  }).fail(function(err) {
+}); // $.each
+
+}).fail(function(err) {
     throw err;
-  });
-
-
- })
+})
+.always(function(){
+$('.loading').hide();
 });
 
+});
+
+});
 
